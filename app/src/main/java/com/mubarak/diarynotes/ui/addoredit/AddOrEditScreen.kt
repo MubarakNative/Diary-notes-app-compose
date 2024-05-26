0dc6972fcac6e157af14836fe457b7feeebc1b8c
Mubarak <enpisb251@gmail.com>
2024-05-26 11:46:51 +0530

    Defined AddEdit and Archive screens

--- app/src/main/java/com/mubarak/diarynotes/ui/addoredit/AddOrEditScreen.kt
+++ app/src/main/java/com/mubarak/diarynotes/ui/addoredit/AddOrEditScreen.kt
@@ -1,9 +1,91 @@
 package com.mubarak.diarynotes.ui.addoredit
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mubarak.diarynotes.R
+import com.mubarak.diarynotes.ui.theme.DiaryTheme
 
 @Composable
 fun AddEditScreen(modifier: Modifier = Modifier) {
 
+    DiaryTheme {
+        Scaffold(modifier = modifier, topBar = {
+            AddEditTopAppBar()
+        }) {
+            DiaryNoteFields(modifier = Modifier.padding(it))
+        }
+    }
+}
+
+@Composable
+fun DiaryNoteFields(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    onTitleChange: (String) -> Unit = {},
+    description: String = "",
+    onDescriptionChange: (String) -> Unit = {}
+) {
+    Column(modifier = modifier) {
+        TextField(
+            value = title,
+            onValueChange = onTitleChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            label = {
+                Text(text = "Title")
+            },
+        )
+
+        TextField(
+            value = description,
+            onValueChange = onDescriptionChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            label = {
+                Text(text = "Description")
+            },
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddEditTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(title = {
+
+    }, modifier = modifier, navigationIcon = {
+
+        IconButton(onClick = { /*TODO: NavController.POPBACKSTACK*/ }) {
+
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = stringResource(id = R.string.nav_back)
+            )
+        }
+    })
+}
+
+@Preview
+@Composable
+private fun AddEditScreenPreview() {
+    DiaryTheme {
+        AddEditScreen()
+    }
 }
---
+++ app/src/main/java/com/mubarak/diarynotes/ui/archive/ArchiveScreen.kt
@@ -0,0 +1,98 @@
+package com.mubarak.diarynotes.ui.archive
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mubarak.diarynotes.ui.note.DiaryNoteItem
+import com.mubarak.diarynotes.ui.theme.DiaryTheme
+
+@Composable
+fun ArchiveScreen(modifier: Modifier = Modifier) {
+
+    DiaryTheme {
+        Scaffold(modifier = modifier, topBar = {
+            AddEditTopAppBar()
+        }) {
+            LazyDiaryNoteItems(
+                modifier = Modifier.padding(it),
+                noteItems = noteItems
+            )
+        }
+    }
+}
+
+data class NoteItem(
+    val title: String,
+    val description: String
+)
+
+val noteItems = listOf(
+    NoteItem(
+        title = "Title 1",
+        description = "Description 1"
+    ),
+    NoteItem(
+        title = "Title 2",
+        description = "Description 2"
+    ),
+    NoteItem(
+        title = "Title 3",
+        description = "Description 3"
+    )
+)
+
+@Composable
+fun LazyDiaryNoteItems(
+    modifier: Modifier = Modifier,
+    noteItems: List<NoteItem>
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            noteItems
+        ) {
+            DiaryNoteItem(title = it.title, description = it.description)
+        }
+    }
+
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddEditTopAppBar(modifier: Modifier = Modifier) {
+    TopAppBar(title = {
+        Text(text = "Archive")
+    }, modifier = modifier, navigationIcon = {
+
+        IconButton(onClick = { /*TODO Open nav drawer*/ }) {
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = null
+            )
+        }
+    })
+}
+
+@Preview
+@Composable
+private fun AddEditScreenPreview() {
+    DiaryTheme {
+        ArchiveScreen()
+    }
+}
